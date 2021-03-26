7e8f87754d8bf9d812434f2dcec448eea1ee32bd
Erick Freire <erilefr@gmail.com>
2021-03-25 22:50:05 -0300

    BemVindo2
    BemVindo2

---
+++ BemVindo2/src/br/com/erickfreire/bemvindo2/BemVindo2.java
@@ -0,0 +1,17 @@
+package br.com.erickfreire.bemvindo2;
+
+
+/**
+ * Programa que exibe uma mensagem de boas vindas formatado com duas linhas de comandos
+ * @author Erick Freire
+ * @version 1 Criado em 25-03-2021 as 22:48
+ *
+ */
+
+public class BemVindo2 {
+	public static void main(String[] args) {
+		System.out.print("Bem Vindo ");
+		System.out.println("a programação em Java!");
+	}
+
+}
